ecf3266864099c31119fa9e102a59770f0265538
Josh Cobert <jdcobert@gmail.com>
2025-05-23 18:44:16 -0400

    add exit link to studio nav

--- sanity.config.ts
+++ sanity.config.ts
@@ -11,12 +11,15 @@ import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 
+import StudioNavbar from '@/sanity/components/studio-navbar'
+
 export default defineConfig({
   basePath: '/peach',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
+  studio: { components: { navbar: StudioNavbar } },
   plugins: [
     structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
