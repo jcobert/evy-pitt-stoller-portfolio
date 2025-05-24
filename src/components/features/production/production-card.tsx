bb1d866790033b4c64ff66ecae5684b039b95ab8
Josh Cobert <jdcobert@gmail.com>
2025-05-24 16:06:46 -0400

    update style

--- src/components/features/production/production-card.tsx
+++ src/components/features/production/production-card.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { FaRegCirclePlay } from 'react-icons/fa6'
 
 import { formatDate } from '@/utils/date'
 import { cn } from '@/utils/style'
@@ -14,21 +15,34 @@ const ProductionCard: FC<Props> = ({ production, className }) => {
   const { title, datePublished } = production || {}
 
   return (
-    <div className={cn('w-full flex flex-col items-center gap-2', className)}>
-      <div className='w-11/12 sm:w-72 max-w-full aspect-video bg-primary object-cover rounded-sm p-px'>
+    <button
+      type='button'
+      disabled
+      className={cn('group w-full flex flex-col items-center gap-2', className)}
+    >
+      <div
+        className={cn(
+          'w-11/12 sm:w-72 max-w-full aspect-video bg-accent object-cover rounded-sm p-px',
+          'relative flex items-center justify-center',
+          'group-hover:shadow-sm transition'
+        )}
+      >
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={null!} alt='' className='rounded-sm size-full' />
+        <FaRegCirclePlay className='absolute text-5xl text-black/40' />
       </div>
 
       <div className='flex flex-col gap-2 items-center text-pretty'>
-        <span>{title}</span>
+        <span className='group-hover:text-foreground/80 transition'>
+          {title}
+        </span>
         {datePublished ? (
-          <span className='text-sm text-muted-foreground'>
+          <span className='text-sm text-muted-foreground group-hover:text-muted-foreground/80 transition'>
             {formatDate(datePublished)}
           </span>
         ) : null}
       </div>
-    </div>
+    </button>
   )
 }
 
