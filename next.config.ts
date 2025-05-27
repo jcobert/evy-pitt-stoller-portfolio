4eb8aa9c999960769bd9b71664d2c4448e2ad7a6
Josh Cobert <jdcobert@gmail.com>
2025-05-27 00:43:15 -0400

    update next config img srcs

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,9 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-      },
+      { protocol: 'https', hostname: 'cdn.sanity.io' },
+      { protocol: 'https', hostname: 'img.youtube.com' },
+      { protocol: 'https', hostname: 'i.vimeocdn.com' },
     ],
   },
 }
